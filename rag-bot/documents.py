b101abeef68c41b1c5f5e1f49091b55430303b10
pavan soma <somasaipavan2000@gmail.com>
2026-01-18 19:42:02 +0530

    documents.py

---
+++ rag-bot/documents.py
@@ -0,0 +1,9 @@
+documents=[
+        {"id": 1,
+         "text": "god is who give life to someone, i got life because of you. so god is there."
+         },
+
+        {"id": 2,
+         "text": "jenkins issue usually comes, iam dont have skills to explain."
+         }
+        ]
